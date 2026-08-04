33f7ff07dc57b28f0bccd4d3f4f0f96c856f3c73
Joseph <joseph.soufsaf@umontreal.ca>
2026-08-04 01:48:01 -0400

    added content, tag and note parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,2 @@
-import Text.XHtml (content)
-import GHC.Data.ShortText (ShortText(contents))
-main :: IO ()
-main = do
-    contents <- readFile "wordlist.txt"
-    putStrLn contents
 
-
-
+    
--- src/Parser/parser.hs
+++ src/Parser/parser.hs
@@ -1,3 +1,36 @@
-sum :: [Int] -> Int
-sum [] = 0
-sum (x:xs) = x + sum xs
+import Text.Read (Lexeme(String))
+import Language.Haskell.TH (Type(ConT), parS)
+data Note = Note Content (Maybe Tag)
+data Content = Content String
+data Tag = Tag String
+
+
+    
+
+parseContentString :: String -> String 
+parseContentString (x:xs) 
+    | x == '@' = []
+    | otherwise = x : parseContentString xs
+
+
+parseContent :: String -> Content
+parseContent str = Content (parseContentString str)
+
+
+parseTag :: String -> Maybe Tag
+parseTag [] = Nothing
+parseTag  (x:xs)
+    | x == '@' = Just (Tag xs) 
+    | otherwise = parseTag xs 
+
+
+
+parseNote :: String  -> Note 
+parseNote str = Note (parseContent str) (parseTag str)
+
+
+
+
+
+
+
