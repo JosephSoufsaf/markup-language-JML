d8ac89088a618d0fe20663c4362bf7f0cce48cba
Joseph <joseph.soufsaf@umontreal.ca>
2026-08-25 02:11:26 -0400

    added chronological sorting to flat(map) view

--- src/parser.hs
+++ src/parser.hs
@@ -1,10 +1,4 @@
-module Parser (parseDocument, splitLines, findViewValue, findSortValue) where
-import AST
-
-
-
 --------------  START OF LINE SPLITTING FUNCTIONS --------------
--- Seperates lines and puts each line into an array
 -- Example input:  "#view:tree\nBuy milk @shopping\nFinish JML parser @todo\nNo tag here"
 -- Example output: ["#view:tree", "Buy milk @shopping", "Finish JML parser @todo", "No tag here"]
 splitLines :: String -> [String]
@@ -39,8 +33,6 @@ removeEmptyLines (x:xs)
 --------------  END OF LINE SPLITTING FUNCTIONS --------------
 
 
-
-
 -------------- START OF HEADER FUNCTIONS --------------
 -- Example Input: ["#view:tree", "Buy milk @shopping", "Finish JML parser @todo", "No tag here"]
 -- Example Output: ["Buy milk @shopping", "Finish JML parser @todo", "No tag here"]
@@ -50,7 +42,10 @@ removeHeaders (x:xs)
     | containsHeader x = removeHeaders xs
     | otherwise = x : removeHeaders xs
 
--- Helper functions for the header
+-- Example Input: "#view:tree"
+-- Example Output: True
+-- Example Input: "Buy milk @shopping"
+-- Example Output: False
 containsHeader :: String -> Bool
 containsHeader [] = False
 containsHeader (x:xs)
@@ -58,6 +53,8 @@ containsHeader (x:xs)
             | otherwise = False 
 
 
+-- Example Input: "#view:tree"
+-- Example Output: "view"
 getKey :: String -> String
 getKey [] = []
 getKey (x:xs)
@@ -65,12 +62,18 @@ getKey (x:xs)
     | x == ':'  = []
     | otherwise = x : getKey xs
 
+-- Example Input: "#view:tree"
+-- Example Output: "tree"
 getValue :: String -> String
 getValue [] = []
 getValue (x:xs)
     | x == ':'  = xs
     | otherwise = getValue xs
 
+-- Example Input: ["#view:tree", "#sort:alphabetical"]
+-- Example Output: "tree"
+-- Example Input: []
+-- Example Output: "flat"
 findViewValue :: [String] -> String
 findViewValue [] = "flat"
 findViewValue (line:rest)
@@ -78,6 +81,10 @@ findViewValue (line:rest)
     | otherwise = findViewValue rest
 
 
+-- Example Input: ["#view:tree", "#sort:alphabetical"]
+-- Example Output: "alphabetical"
+-- Example Input: []
+-- Example Output: "alphabetical"
 findSortValue :: [String] -> String
 findSortValue [] = "alphabetical"
 findSortValue (line:rest)
@@ -89,21 +96,21 @@ findSortValue (line:rest)
 
 -------------- START OF INDEX ASSIGNING FUNCTIONS --------------
 -- Example Input: ["Buy milk @shopping", "Finish JML parser @todo", "No tag here"]
--- Example Output: [(1, "Buy milk @shopping", (2, "Finish JML parser @todo"), (3, "No tag here")]
+-- Example Output: [(0, "Buy milk @shopping"), (1, "Finish JML parser @todo"), (2, "No tag here")]
 assignIndex :: [String] -> [(Int, String)]
 assignIndex lines = (zip [0..] lines)
 
 -------------- END OF INDEX ASSIGNING FUNCTIONS --------------
 
-    
+
 -------------- START OF PARSING FUNCTIONS --------------
 
--- Example Input: (1, "Buy Milk @shopping")
--- Example Output : Content 1 "Buy Milk"
+-- Example Input: (0, "Buy milk @shopping")
+-- Example Output: Content 0 "Buy milk "
 parseContent :: (Int, String) -> Content
 parseContent (index, line) = Content index (getContentString line) where
-    -- Example input:  (1, "Buy milk @shopping")
-    -- Example output: Content 1 "Buy Milk"
+    -- Example Input: "Buy milk @shopping"
+    -- Example Output: "Buy milk "
     getContentString :: String -> String 
     getContentString [] = []
     getContentString (x:xs) 
@@ -111,7 +118,6 @@ parseContent (index, line) = Content index (getContentString line) where
         | otherwise = x : getContentString xs
 
 
--- everything AFTER the first @
 -- Example input:  "Buy milk @shopping@walmart"
 -- Example output: "shopping@walmart"
 dropUntilTag :: String -> String
@@ -121,7 +127,6 @@ dropUntilTag (x:xs)
     | otherwise = dropUntilTag xs
 
 
--- wraps each already-split tag string into a Tag
 -- Example input:  ["shopping", "walmart"]
 -- Example output: [Tag "shopping", Tag "walmart"]
 parseTags :: [String] -> [Tag]
@@ -129,8 +134,6 @@ parseTags [] = []
 parseTags (x:xs) = Tag x : parseTags xs
 
 
--- getTags and parseTags work together
--- splits a string on every remaining @
 -- Example input:  "shopping@walmart"
 -- Example output: ["shopping", "walmart"]
 getTags :: String -> [String]
@@ -154,11 +157,15 @@ getTags str = firstTag str : getTags (remainingTags str) where
         | otherwise = remainingTags xs
 
 
+-- Example Input: [Tag "shopping "]
+-- Example Output: [Tag "shopping"]
 trimTags :: [Tag] -> [Tag]
 trimTags [] = []
 trimTags ((Tag name):xs) = Tag (trimEdges name) : trimTags xs
 
 
+-- Example Input: "shopping "
+-- Example Output: "shopping"
 trimEdges :: String -> String
 trimEdges str = reverse (removeEmptySpace (reverse (removeEmptySpace str))) where
     removeEmptySpace :: String -> String
@@ -168,23 +175,21 @@ trimEdges str = reverse (removeEmptySpace (reverse (removeEmptySpace str))) wher
         | otherwise = c : cs
 
 
--- Example Input: [(1, "Buy milk @shopping")]
--- Example output: Note (Content 1 "Buy milk ") [Tag "shopping"]
+-- Example Input: (0, "Buy milk @shopping")
+-- Example output: Note (Content 0 "Buy milk ") [Tag "shopping"]
 parseNote :: (Int, String) -> Note
 parseNote (index, line) = Note (parseContent (index, line)) (trimTags (parseTags (removeEmptyLines (getTags (dropUntilTag line)))))
 
 
-
--- Example input:  [(1, "Buy milk @shopping"), (2, "No tag here")]
--- Example output: [Note (Content 1 "Buy milk ") [Tag "shopping"], Note (Content 2 "No tag here") []]
+-- Example input:  [(0, "Buy milk @shopping"), (1, "No tag here")]
+-- Example output: [Note (Content 0 "Buy milk ") [Tag "shopping"], Note (Content 1 "No tag here") []]
 parseNotes :: [(Int, String)] -> [Note]
 parseNotes [] = []
 parseNotes (x:xs) = parseNote x : parseNotes xs
 
 -- Example input:  "Buy milk @shopping\nNo tag here"
--- Example output: Document [Note (Content "Buy milk ") [Tag "shopping"], Note (Content "No tag here") []]
+-- Example output: Document [Note (Content 0 "Buy milk ") [Tag "shopping"], Note (Content 1 "No tag here") []]
 parseDocument :: String -> Document
 parseDocument documentContent = Document (parseNotes (assignIndex (removeHeaders (removeEmptyLines (splitLines documentContent)))))
-
 
 -------------- END OF PARSING FUNCTIONS --------------
